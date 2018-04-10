22fc9a942c028d22f22b13c75ca4c6425023c675
Viktor <viktor.leher@gmail.com>
2018-04-10 11:37:52 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/CFLOP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CFLOP.dir/main.cpp.o"
+  "CFLOP.pdb"
+  "CFLOP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CFLOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
