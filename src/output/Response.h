acddab60bdac0f99b2dd886d147e3c02f5c4762f
Viktor <viktor.leher@gmail.com>
2018-04-29 16:38:20 +0200

    added output-lib basics

---
+++ src/output/Response.h
@@ -0,0 +1,8 @@
+//
+// Created by vik on 29.04.18.
+//
+
+#ifndef CFLOP_RESPONSE_H
+#define CFLOP_RESPONSE_H
+
+#endif //CFLOP_RESPONSE_H
---
+++ src/output/response_types.h
@@ -0,0 +1,59 @@
+//
+// Created by vik on 29.04.18.
+//
+
+#ifndef CFLOP_RESPONSE_TYPES_H
+#define CFLOP_RESPONSE_TYPES_H
+
+//TODO: to/from json + structs mit entsprechenden daten füllen
+
+struct nodes_post_r_s {
+
+};
+
+struct nodes_delete_r_s {
+
+};
+
+struct nodes_get_r_s {
+
+};
+
+struct nodes_put_start_r_s {
+
+};
+
+struct nodes_put_end_r_s {
+
+};
+
+struct edges_get_r_s {
+
+};
+
+struct edges_post_r_s {
+
+};
+
+struct edges_delete_r_s {
+
+};
+
+struct state_get_r_s {
+
+};
+
+struct state_post_r_s {
+
+};
+
+struct state_put_r_s {
+
+};
+
+struct failure_r_s {
+    std::string failure;
+};
+
+
+#endif //CFLOP_RESPONSE_TYPES_H
