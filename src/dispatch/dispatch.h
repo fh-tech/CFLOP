30d489a33f68215d32dfcbfe193b11e7769cbe6e
Viktor <viktor.leher@gmail.com>
2018-04-29 16:42:51 +0200

    basis for dispatch-lib

---
+++ src/dispatch/dispatch.h
@@ -0,0 +1,11 @@
+//
+// Created by vik on 29.04.18.
+//
+
+#ifndef CFLOP_DISPATCH_H
+#define CFLOP_DISPATCH_H
+
+
+//TODO: function that takes a Request Object, a FinalStateMachine Object and makes a Response object out of it
+
+#endif //CFLOP_DISPATCH_H
