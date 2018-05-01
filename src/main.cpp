94c7d27811985c5fc6cf7a3ff516a72e6c47a9d0
Daniel Kudernatsch <d.kudernatsch@outlook.com>
2018-05-01 19:31:19 +0200

    naive cflop main loop impl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+#include <dispatch.h>
+#include <input.h>
+#include <output.h>
+
+namespace in = input_lib;
+namespace out = output_lib;
+namespace dispatch = dispatch_lib;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    in::Request r;
+    fsm::FinalStateMachine fsm{};
+    while (true){
+
+        json j_in;
+        std::cin >> j_in;
+        r = j_in;
+        auto response = dispatch::dispatch(r, fsm);
+        json j_out = response;
+        std::cout << j_out;
+    }
+
 }
